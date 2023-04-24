6da0bd6d3dfdc902de8ffe962f13ac936286e9bd
martinaemunoz <martinaemunoz@gmail.com>
2023-04-23 23:52:28 -0400

    cipher method and css updates

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,26 @@
 const cipher = {
-  // ...
+  encode: function(offset_value1,cipherMessage){
+    let result = '';
+    for (let i = 0; i < cipherMessage.length; i++) {
+      const ASCIInum = cipherMessage[i].charCodeAt();
+      console.log(ASCIInum);
+      if (ASCIInum>=65 && ASCIInum<=90){
+        result += String.fromCharCode(((ASCIInum-65 + offset_value1)%26)+65);}
+    } return result; 
+  },
+
+decode: function(offset_value2, decipherMessage){
+  let result = '';
+  while (offset_value2 > 26){
+    offset_value2 -= 26;
+  }
+  for (let i = 0; i < decipherMessage.length; i++){
+    const ASCIInum = decipherMessage[i].charCodeAt();
+    if (ASCIInum>=65 && ASCIInum<=90){
+      result += String.fromCharCode(((ASCIInum-65 - offset_value2 + 26)%26)+65);
+    }
+  } return result;
+}
 };
 
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -7,21 +7,21 @@ const selectDecipher = document.getElementById("select_decipher");
 const btnReturn1 = document.getElementById("return1");
 const btnReturn2 = document.getElementById("return2");
 
-//llamando a la función del botón cifrar para ingresar al menú de cipher
+//declarando la función del botón cifrar para ingresar al menú de cipher
 function cipherMenu(){
     mainBody.style.display = "none";
     document.getElementById("cipher_body").style.display = "block";
 }
 selectCipher.addEventListener("click", cipherMenu);
 
-//llamando a la función del botón descifrar para ingresar al menú de decipher
+//declarando la función del botón descifrar para ingresar al menú de decipher
 function decipherMenu(){
     mainBody.style.display = "none";
     document.getElementById("decipher_body").style.display = "block";
 }
 selectDecipher.addEventListener("click", decipherMenu);
 
-//llamando a la función de los botones volver para regresar al menú princial
+//declarando la función de los botones volver para regresar al menú princial
 function btnReturn(){
     cipher_body.style.display = 'none';
     decipher_body.style.display = 'none';
@@ -30,4 +30,27 @@ function btnReturn(){
 btnReturn1.addEventListener('click', btnReturn);
 btnReturn2.addEventListener('click', btnReturn);
 
+
+//llamando el método encode
+const btnCipher = document.getElementById("btn_cipher");
+const cipherResult = document.getElementById("result1"); 
+
+btnCipher.addEventListener("click", (event) => {
+    const cipherMessage = document.getElementById("user_input1").value;
+    const offset_value1 = parseInt(document.getElementById("range1").value);
+    console.log(cipherMessage);
+    console.log(offset_value1);
+    cipherResult.value = cipher.encode(offset_value1,cipherMessage);
+}
+);
  
+//llamando el método decode
+const btnDecipher = document.getElementById("btn_decipher");
+const decipherResult = document.getElementById("result2");
+
+btnDecipher.addEventListener("click", (event) =>{
+    const decipherMessage = document.getElementById("user_input2").value;
+    const offset_value2 = parseInt(document.getElementById("range2").value);
+    decipherResult.value = cipher.decode(offset_value2,decipherMessage);
+}
+);
