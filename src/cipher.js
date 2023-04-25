5f783b663c0e672657174d1068bd6e681d7ed66d
martinaemunoz <martinaemunoz@gmail.com>
2023-04-25 19:21:19 -0400

    npm tests running

--- src/cipher.js
+++ src/cipher.js
@@ -1,26 +1,31 @@
 const cipher = {
   encode: function(offset_value1,cipherMessage){
+    if (typeof offset_value1 !== "number" || typeof cipherMessage !== "string"){
+      throw new TypeError ('Invalid arguments: offset must be a number and txt must be a string')
+    }
     let result = '';
     for (let i = 0; i < cipherMessage.length; i++) {
       const ASCIInum = cipherMessage[i].charCodeAt();
-      console.log(ASCIInum);
       if (ASCIInum>=65 && ASCIInum<=90){
         result += String.fromCharCode(((ASCIInum-65 + offset_value1)%26)+65);}
     } return result; 
   },
 
-decode: function(offset_value2, decipherMessage){
-  let result = '';
-  while (offset_value2 > 26){
-    offset_value2 -= 26;
-  }
-  for (let i = 0; i < decipherMessage.length; i++){
-    const ASCIInum = decipherMessage[i].charCodeAt();
-    if (ASCIInum>=65 && ASCIInum<=90){
-      result += String.fromCharCode(((ASCIInum-65 - offset_value2 + 26)%26)+65);
+  decode: function (offset_value2, decipherMessage) {
+    if (typeof offset_value2 !== "number" || typeof decipherMessage !== "string"){
+      throw new TypeError ('Invalid arguments: offset must be a number and txt must be a string')
     }
-  } return result;
-}
+    let result = '';
+    while (offset_value2 > 26){
+      offset_value2 -= 26;
+    }
+    for (let i = 0; i < decipherMessage.length; i++){
+      const ASCIInum = decipherMessage[i].charCodeAt();
+      if (ASCIInum>=65 && ASCIInum<=90){
+        result += String.fromCharCode(((ASCIInum-65 - offset_value2 + 26)%26)+65);
+      }
+    } return result;
+  }
 };
 
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -9,23 +9,23 @@ const btnReturn2 = document.getElementById("return2");
 
 //declarando la función del botón cifrar para ingresar al menú de cipher
 function cipherMenu(){
-    mainBody.style.display = "none";
-    document.getElementById("cipher_body").style.display = "block";
+  mainBody.style.display = "none";
+  document.getElementById("cipher_body").style.display = "block";
 }
 selectCipher.addEventListener("click", cipherMenu);
 
 //declarando la función del botón descifrar para ingresar al menú de decipher
 function decipherMenu(){
-    mainBody.style.display = "none";
-    document.getElementById("decipher_body").style.display = "block";
+  mainBody.style.display = "none";
+  document.getElementById("decipher_body").style.display = "block";
 }
 selectDecipher.addEventListener("click", decipherMenu);
 
 //declarando la función de los botones volver para regresar al menú princial
 function btnReturn(){
-    cipher_body.style.display = 'none';
-    decipher_body.style.display = 'none';
-    mainBody.style.display = 'block';
+  document.getElementById("cipher_body").style.display = 'none';
+  document.getElementById("decipher_body").style.display = 'none';
+  mainBody.style.display = 'block';
 }
 btnReturn1.addEventListener('click', btnReturn);
 btnReturn2.addEventListener('click', btnReturn);
@@ -35,12 +35,10 @@ btnReturn2.addEventListener('click', btnReturn);
 const btnCipher = document.getElementById("btn_cipher");
 const cipherResult = document.getElementById("result1"); 
 
-btnCipher.addEventListener("click", (event) => {
-    const cipherMessage = document.getElementById("user_input1").value;
-    const offset_value1 = parseInt(document.getElementById("range1").value);
-    console.log(cipherMessage);
-    console.log(offset_value1);
-    cipherResult.value = cipher.encode(offset_value1,cipherMessage);
+btnCipher.addEventListener("click", () => {
+  const cipherMessage = document.getElementById("user_input1").value;
+  const offset_value1 = parseInt(document.getElementById("range1").value);
+  cipherResult.value = cipher.encode(offset_value1,cipherMessage);
 }
 );
  
@@ -48,9 +46,9 @@ btnCipher.addEventListener("click", (event) => {
 const btnDecipher = document.getElementById("btn_decipher");
 const decipherResult = document.getElementById("result2");
 
-btnDecipher.addEventListener("click", (event) =>{
-    const decipherMessage = document.getElementById("user_input2").value;
-    const offset_value2 = parseInt(document.getElementById("range2").value);
-    decipherResult.value = cipher.decode(offset_value2,decipherMessage);
+btnDecipher.addEventListener("click", () =>{
+  const decipherMessage = document.getElementById("user_input2").value;
+  const offset_value2 = parseInt(document.getElementById("range2").value);
+  decipherResult.value = cipher.decode(offset_value2,decipherMessage);
 }
 );
